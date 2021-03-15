94b216e61126a7d8e9ca14827d2b1160513b7b81
ilyass <tahzima.i@gmail.com>
2021-03-15 10:44:15 +0100

    delete author

--- authorPHP.php
+++ authorPHP.php
@@ -32,12 +32,8 @@ if(isset($_POST['btn'])){
 
       //Delete :
        if(isset($_GET['id'])){
-         $requeste="SELECT idBook FROM bookauthor WHERE idAuthor='".$_GET['id']."'";
+         $requeste="DELETE FROM book WHERE id IN (SELECT idBook from bookauthor where idAuthor='".$_GET['id']."')";
          $result=mysqli_query($connect,$requeste);
-         while($row=$result->fetch_assoc()){
-         $requete="DELETE FROM book WHERE id='".$row['idBook']."'";
-         $result=mysqli_query($connect,$requete);
-        }
          $query="DELETE FROM author WHERE  id= '".$_GET['id']."'";
          $result=mysqli_query($connect,$query);
        }
