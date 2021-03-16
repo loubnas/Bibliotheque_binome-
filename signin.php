66210dc8855e9e9cc06d6e33f56f9c45d14cc305
ilyass <tahzima.i@gmail.com>
2021-03-16 12:08:17 +0100

    signIn

--- signin.php
+++ signin.php
@@ -3,11 +3,29 @@
 <head>
 	<title>Login</title>
 	<link rel="stylesheet" type="text/css" href="css/signin.css">
+    <link rel="stylesheet" type="text/css" href="css/styles.css"> 
 	<link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
 	<script src="https://kit.fontawesome.com/a81368914c.js"></script>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
+	<header>	
+		<svg class="iconenav" viewBox="0 0 100 80" width="40" height="40" fill="#2DA07A" onclick="nav()">
+			<rect width="100" height="20"></rect>
+			<rect y="30" width="100" height="20"></rect>
+			<rect y="60" width="100" height="20"></rect>
+		</svg> 
+		<img src="images/LOGO.png">
+		<ul id="navcontenu" class=" " vue="" >
+				<li > <a href="index.php" class="active">Home</a> </li>
+				<li> <a href="gallery.php"> <span class="titlegallery">Gallery</span></a> </li>
+				<li><a href="book.php"><span class="titlebooks">Books </span></a></li>
+				<li > <a href="authors.php"><span class="titleauthors">Authors</span></a></li>
+				<li><a href="signUp.php"><input type="submit" value="Sign up" name="Sign up" ></a> </li> 
+				<li><a href="signin.php"><input type="submit" value="Sign in" name="Sign in"></a> </li>
+			
+			</ul>                            	 	
+	</header>
 	<img class="wave" src="images/wave.png">
 	<div class="container">
 		<div class="img">
